e72156c0fe1364e59577eefc45c11eca0b8807c8
Andrea Carraro <me@andreacarraro.it>
2023-09-05 08:48:37 +0200

    test: add test for serialized strings

---
+++ test/serialization.test.ts
@@ -0,0 +1,34 @@
+import path from 'path';
+import { describe, it, expect } from 'vitest';
+import { openapiToTsJsonSchema } from '../src';
+import { importFresh } from './test-utils';
+
+const fixtures = path.resolve(__dirname, 'fixtures');
+
+describe('openapiToTsJsonSchema', async () => {
+  it('serializes strings as expected', async () => {
+    const { outputPath } = await openapiToTsJsonSchema({
+      openApiSchema: path.resolve(fixtures, 'serialization/specs.yaml'),
+      definitionPathsToGenerateFrom: ['components.schemas'],
+      silent: true,
+    });
+
+    const actual = await importFresh(
+      path.resolve(outputPath, 'components/schemas/Foo'),
+    );
+
+    const expected = {
+      description: 'Foo description',
+      type: 'object',
+      properties: {
+        one: {
+          type: 'string',
+          pattern1: "([a-zA-Z\\s\\-']){1,50}",
+          pattern2: "([a-zA-Z\\s\\-']){1,50}",
+        },
+      },
+    };
+
+    expect(actual.default).toEqual(expected);
+  });
+});
