4508d37adf4423eb4d2c03a4dccde3aeb00493fd
Andrea Carraro <toomuchdesign@users.noreply.github.com>
2023-09-25 13:33:00 +0200

    test: review external schemas tests (#94)

--- test/externalRefs.test.ts
+++ test/externalRefs.test.ts
@@ -3,115 +3,112 @@ import { describe, it, expect } from 'vitest';
 import { fixtures, makeTestOutputPath } from './test-utils';
 import { openapiToTsJsonSchema } from '../src';
 
-describe('External $ref', () => {
-  it('Resolves external refs', async () => {
-    const { outputPath } = await openapiToTsJsonSchema({
-      openApiSchema: path.resolve(fixtures, 'external-ref/specs.yaml'),
-      outputPath: makeTestOutputPath('external-refs'),
-      definitionPathsToGenerateFrom: ['components.schemas'],
-      silent: true,
-    });
+describe('External $refs', () => {
+  describe('refHandling option === "inline"', () => {
+    it('Resolves external refs', async () => {
+      const { outputPath } = await openapiToTsJsonSchema({
+        openApiSchema: path.resolve(fixtures, 'external-ref/specs.yaml'),
+        outputPath: makeTestOutputPath('external-refs'),
+        definitionPathsToGenerateFrom: ['components.schemas'],
+        silent: true,
+      });
 
-    // $ref: './external-definition.yaml#/components/schemas/Foo'
-    const externalDefinitionWithRefSchema = await import(
-      path.resolve(outputPath, 'components/schemas/ExternalDefinitionWithRef')
-    );
+      // $ref: './external-definition.yaml#/components/schemas/Foo'
+      const externalDefinitionWithRefSchema = await import(
+        path.resolve(outputPath, 'components/schemas/ExternalDefinitionWithRef')
+      );
 
-    expect(externalDefinitionWithRefSchema.default).toEqual({
-      description: 'External Foo description',
-      type: ['string', 'null'],
-      enum: ['yes', 'no', null],
-    });
+      expect(externalDefinitionWithRefSchema.default).toEqual({
+        description: 'External Foo description',
+        type: ['string', 'null'],
+        enum: ['yes', 'no', null],
+      });
 
-    // $ref: './external-definition-whole-document.yaml'
-    const externalDefinitionWholeDocumentSchema = await import(
-      path.resolve(
-        outputPath,
-        'components/schemas/ExternalDefinitionWholeDocument',
-      )
-    );
+      // $ref: './external-definition-whole-document.yaml'
+      const externalDefinitionWholeDocumentSchema = await import(
+        path.resolve(
+          outputPath,
+          'components/schemas/ExternalDefinitionWholeDocument',
+        )
+      );
 
-    expect(externalDefinitionWholeDocumentSchema.default).toEqual({
-      description: 'External definition whole document',
-      type: ['string', 'null'],
-      enum: ['yes', 'no', null],
-    });
+      expect(externalDefinitionWholeDocumentSchema.default).toEqual({
+        description: 'External definition whole document',
+        type: ['string', 'null'],
+        enum: ['yes', 'no', null],
+      });
 
-    // $ref: './external-definition-nested-refs.yaml#/components/schemas/BarFromRef'
-    const externalDefinitionNestedRefsSchema = await import(
-      path.resolve(
-        outputPath,
-        'components/schemas/ExternalDefinitionNestedRefs',
-      )
-    );
+      // $ref: './external-definition-nested-refs.yaml#/components/schemas/BarFromRef'
+      const externalDefinitionNestedRefsSchema = await import(
+        path.resolve(
+          outputPath,
+          'components/schemas/ExternalDefinitionNestedRefs',
+        )
+      );
 
-    expect(externalDefinitionNestedRefsSchema.default).toEqual({
-      description: 'External Bar description',
-      type: ['string', 'null'],
-      enum: ['yes', 'no', null],
-    });
+      expect(externalDefinitionNestedRefsSchema.default).toEqual({
+        description: 'External Bar description',
+        type: ['string', 'null'],
+        enum: ['yes', 'no', null],
+      });
 
-    // Local definition referencing previous external schemas
-    const localDefinitionReferencingExternalSchemas = await import(
-      path.resolve(
-        outputPath,
-        'components/schemas/LocalDefinitionReferencingExternal',
-      )
-    );
+      // Local definition referencing previous external schemas
+      const localDefinitionReferencingExternalSchemas = await import(
+        path.resolve(
+          outputPath,
+          'components/schemas/LocalDefinitionReferencingExternal',
+        )
+      );
 
-    expect(localDefinitionReferencingExternalSchemas.default).toEqual({
-      type: 'object',
-      properties: {
-        externalDefinitionWithRef: externalDefinitionWithRefSchema.default,
-        externalDefinitionWholeDocument:
-          externalDefinitionWholeDocumentSchema.default,
-        externalDefinitionNestedRefs:
-          externalDefinitionNestedRefsSchema.default,
-      },
+      expect(localDefinitionReferencingExternalSchemas.default).toEqual({
+        type: 'object',
+        properties: {
+          externalDefinitionWithRef: externalDefinitionWithRefSchema.default,
+          externalDefinitionWholeDocument:
+            externalDefinitionWholeDocumentSchema.default,
+          externalDefinitionNestedRefs:
+            externalDefinitionNestedRefsSchema.default,
+        },
+      });
     });
   });
 
-  describe('refHandling option === "import"', async () => {
-    // @NOTE this feature has not been implemented
+  // @NOTE this feature has not been implemented
+  describe('refHandling option === "import"', () => {
     describe('openAPI definitions imported multiple times', () => {
-      it.fails.each([
-        {
+      it.fails('dedupe imports and resolve against same schema', async () => {
+        const { outputPath } = await openapiToTsJsonSchema({
+          openApiSchema: path.resolve(fixtures, 'external-ref/specs.yaml'),
+          outputPath: makeTestOutputPath('external-refs'),
+          definitionPathsToGenerateFrom: ['components.schemas'],
           refHandling: 'import',
-        },
-        {
-          refHandling: 'keep',
-        },
-      ] as const)(
-        'generates expected relative path',
-        async ({ refHandling }) => {
-          const { outputPath } = await openapiToTsJsonSchema({
-            openApiSchema: path.resolve(fixtures, 'external-ref/specs.yaml'),
-            outputPath: makeTestOutputPath('external-refs'),
-            definitionPathsToGenerateFrom: ['components.schemas'],
-            refHandling,
-            silent: true,
-          });
+          silent: true,
+        });
 
-          const externalDefinitionWithRefSchema = await import(
-            path.resolve(
-              outputPath,
-              'components/schemas/ExternalDefinitionWithRef',
-            )
-          );
+        // @NOTE What would be the $ref value of an external definition?
+        const externalDefinitionWithRefSchema = await import(
+          path.resolve(
+            outputPath,
+            'components/schemas/ExternalDefinitionWithRef',
+          )
+        );
 
-          const externalDefinitionWithSameRefSchema = await import(
-            path.resolve(
-              outputPath,
-              'components/schemas/ExternalDefinitionWithSameRef',
-            )
-          );
+        const externalDefinitionWithSameRefSchema = await import(
+          path.resolve(
+            outputPath,
+            'components/schemas/ExternalDefinitionWithSameRef',
+          )
+        );
 
-          // Same imported schemas should be resolved against the same entity (reference equality)
-          expect(externalDefinitionWithRefSchema.default).toBe(
-            externalDefinitionWithSameRefSchema.default,
-          );
-        },
-      );
+        // Same imported schemas should be resolved against the same entity (reference equality)
+        expect(externalDefinitionWithRefSchema.default).toBe(
+          externalDefinitionWithSameRefSchema.default,
+        );
+      });
     });
+
+    // @NOTE this feature has not been implemented
+    // @NOTE This test should be defined once decided what of the $ref value of external definitions
+    // describe('refHandling option === "keep"', () => {});
   });
 });
