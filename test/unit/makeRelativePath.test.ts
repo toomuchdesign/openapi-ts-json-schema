03c11809d774c81d04747802c68b6bffab10ebd6
Andrea Carraro <me@andreacarraro.it>
2024-03-04 20:01:00 +0100

    test: fix metadata test on win

--- test/unit/makeRelativePath.test.ts
+++ test/unit/makeRelativePath.test.ts
@@ -1,5 +1,5 @@
-import { describe, it, expect } from 'vitest';
 import path from 'node:path';
+import { describe, it, expect } from 'vitest';
 import { makeRelativePath } from '../../src/utils';
 
 describe('makeRelativePath', () => {
